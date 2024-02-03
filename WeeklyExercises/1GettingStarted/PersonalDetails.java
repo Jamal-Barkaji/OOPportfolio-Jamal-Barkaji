568e8a6417e5a92a85dbccc79b7ea320097933f2
Jamal Barkaji <barkajijamal@gmail.com>
2024-02-03 16:50:14 +0000

    Added Personal Details program

---
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,10 @@
+public class PersonalDetails
+{
+    public static void main(String[] args)
+    {
+        System.out.println("First Name: Bob");
+        System.out.println("Surname: Bobson");
+        System.out.println("Gender: Male");
+        System.out.println("Age: 20");
+    }
+}
