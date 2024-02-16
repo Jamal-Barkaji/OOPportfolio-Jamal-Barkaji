1725edbd2028352cf90a44afdb0c64b1e70e8a40
Jamal Barkaji <barkajijamal@gmail.com>
2024-02-16 17:01:16 +0000

    Added edge case handling

--- WeeklyExercises/2DataAndExpressions/MilesToKilometers.java
+++ WeeklyExercises/2DataAndExpressions/MilesToKilometers.java
@@ -1,19 +1,22 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class MilesToKilometers
-{
-    public static void main(String[] args)
-    {
-        double input;
-        double answer;
-        Scanner scan = new Scanner(System.in);
+public class MilesToKilometers {
+    public static void main(String[] args) {
+        try {
+            double input;
+            double answer;
+            Scanner scan = new Scanner(System.in);
 
 
-        System.out.println("Enter value in miles");
-        input = scan.nextDouble();
-        scan.close();
+            System.out.println("Enter value in miles: ");
+            input = scan.nextDouble();
+            scan.close();
 
-        answer = (double) (input *  1.60935);
-        System.out.println(input + " miles = " + answer + " kilometers");
+            answer = (double) (input * 1.60935);
+            System.out.println(input + " miles = " + answer + " kilometers");
+        } catch (InputMismatchException e) {
+            System.out.println("Error. Inappropriate input.");
+        }
     }
 }
--- WeeklyExercises/2DataAndExpressions/SquareCalculations.java
+++ WeeklyExercises/2DataAndExpressions/SquareCalculations.java
@@ -1,22 +1,30 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class SquareCalculations
-{
-    public static void main(String[] args)
-    {
-        double input;
-        double perimeter;
-        double area;
-        Scanner scan = new Scanner(System.in);
+public class SquareCalculations {
+    public static void main(String[] args) {
+        try {
+            double input;
+            double perimeter;
+            double area;
+            Scanner scan = new Scanner(System.in);
 
 
-        System.out.println("Enter length of a square's side: ");
-        input = scan.nextDouble();
-        scan.close();
+            System.out.println("Enter length of a square's side: ");
+            input = scan.nextDouble();
+            scan.close();
 
-        perimeter = (double) (input *  4);
-        area = (double) (input * input);
-        System.out.println(input + " Perimeter = " + perimeter + " units");
-        System.out.println(input + " Area = " + area + " units^2");
+            area = (double) (input * input);
+
+            if (input < 0)
+                perimeter = (double) (input * 4 * -1);
+            else
+                perimeter = (double) (input * 4);
+
+            System.out.println("Perimeter = " + perimeter + " units");
+            System.out.println("Area = " + area + " units^2");
+        } catch (InputMismatchException e) {
+            System.out.println("Error. Inappropriate input.");
+        }
     }
 }
