0a89d16016ad2b1907f6acb20156941f4aebb01f
Jamal Barkaji <barkajijamal@gmail.com>
2024-02-16 16:52:02 +0000

    Added edge case handling

--- WeeklyExercises/2DataAndExpressions/ComputingAverages.java
+++ WeeklyExercises/2DataAndExpressions/ComputingAverages.java
@@ -5,26 +5,29 @@
 // *******************************************************************
 
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
-public class ComputingAverages
-{
-    public static void main(String[] args)
-    {
-        int val1, val2, val3;
-        double average;
-        Scanner scan = new Scanner(System.in);
+public class ComputingAverages {
+    public static void main(String[] args) {
+        try {
+
+            int val1, val2, val3;
+            double average;
+            Scanner scan = new Scanner(System.in);
 
 
-        // get three values from user
-        System.out.println("Please enter three integers and " +
-                "I will compute their average");
-        int num1 = scan.nextInt();
-        int num2 = scan.nextInt();
-        int num3 = scan.nextInt();
-        scan.close();
+            System.out.println("Please enter three integers and " +
+                    "I will compute their average: ");
+            int num1 = scan.nextInt();
+            int num2 = scan.nextInt();
+            int num3 = scan.nextInt();
+            scan.close();
 
-        average = (double) (num1 + num2 + num3) /3;
-        System.out.println("Average = " + average);
-
+            average = (double) (num1 + num2 + num3) / 3;
+            System.out.println("Average = " + average);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Error. Inappropriate input.");
+        }
     }
 }
